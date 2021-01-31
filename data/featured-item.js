b73387b27a38b67ebeb291214d521de9bacb3a5c
Tino <tinomuzambi@gmail.com>
2021-01-31 20:28:36 +0200

    Removes old data.

--- data/featured-item.js
+++
@@ -1,6 +0,0 @@
-const item = {
-	title: "Would you sacrifice one person to save five? - Eleanor Nelsen",
-	date: "Thursday 31 December 2020",
-	url: "https://www.youtube.com/watch?v=yg16u_bzjPE",
-	description: `The infamous trolley problem. What would you do?`,
-};
